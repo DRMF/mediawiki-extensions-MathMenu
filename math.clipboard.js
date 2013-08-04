c40092b94d19119d01f745602510bb649feea21e
Deyan Ginev <d.ginev@jacobs-university.de>
2013-08-04 15:15:23 -0400

    a fully functional math clipboard

--- math.clipboard.js
+++ math.clipboard.js
@@ -6,45 +6,44 @@ JOBAD.modules.register({
     'author': 'Janelle Williams and Deyan Ginev',
     'description':  'Clipboard for math formulas realized via Zeroclipboard',
   },
+  init: function(){
+    var $saved_text = $("<span id='math_clipboard_saved'>Clipboard Save text</span>");
+    $saved_text.hide();
+    $('body').append($saved_text);
+    var $clip = new ZeroClipboard($saved_text[0], {
+      moviePath: "/extensions/MathMenu/ZeroClipboard.swf",
+    });
+    $clip.setText($saved_text);
+  },
   onEvent: function(evt){
-    if(evt == "contextMenuOpen"){
-    //Context Menu is opened
-    //alert("Menu opened!");
-    var $menu = $("#math_clipboard_menu");
-    var math = menu.closest('math');
-    if (! math.is('math')) { return false;}
-    var $tex = math.find('annotation[encoding="application/x-tex"]').text();
-    // The most important part is here -- attach a Flash ZeroClipboard object
-    // to the current menu row, which will only be created __after__ this function has completed
-    // in other words add the attachment into an event that will be triggered when the row is created
-    ZeroClipboard.setMoviePath('/extensions/JOBAD-git/ZeroClipboard.swf');
-    var clip = new ZeroClipboard.Client(); 
-    clip.glue($menu[0], $menu.parent()[0]);
-    clip.setText("TeXXX");
-    $menu.zclip({
-      path:'/extensions/JOBAD-git/ZeroClipboard.swf',
-      copy:"Tex"});
+    var $text = $("#math_clipboard_saved").text();
+    if(evt == "contextMenuOpen"){ //Context Menu is opened
+      // The most important part is here -- attach a Flash ZeroClipboard object
+      // to the current menu row, which will only be created __after__ this function has completed
+      // in other words add the attachment into an event that will be triggered when the row is created
+      var $menu = $("#math_clipboard_menu");
+      $menu.attr("data-clipboard-ready","true");
+      var $clip = new ZeroClipboard( $menu[0], {
+        moviePath: "/extensions/MathMenu/ZeroClipboard.swf",
+      });
+      $clip.setText($text);
     }
   },
   contextMenuEntries: function(target){
     if(target.is('#nomenu,#nomenu *')){ //no menu for these elements
       return false; }
-    var math = target.closest('math');
-    if (! math.is('math')) { return false;}
-    
-    return [
-      ["Copy TeX Source", [function(element,object){
-        // We really have nothing to do in the actual click,
-        // the Flash will capture that event and do the clipboard magic
-        // Temporary: just so that we do something
-      }],
+    var $math = target.closest('math');
+    if (! $math.is('math')) { return false;}
+    var $tex = $math.find('annotation[encoding="application/x-tex"]').text();
+    $("#math_clipboard_saved").text($tex);
+        
+    return {
+      "Copy TeX Source": [function(element){return true;},
       {
       "id": "math_clipboard_menu", //This is the id
       "icon": "none" //set an icon if desired
       }]
-    ];
+    };
   }
 });
 })(JOBAD.refs.$);
-
-
