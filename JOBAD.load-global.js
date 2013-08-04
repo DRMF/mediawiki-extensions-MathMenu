4314b6083224330e8a701f54cdac66395ffecdf2
Deyan Ginev <d.ginev@jacobs-university.de>
2013-08-04 15:16:21 -0400

    show source is no longer needed when we have the clipboard working

--- JOBAD.load-global.js
+++ JOBAD.load-global.js
@@ -2,7 +2,6 @@
   var selector = jQuery("#mw-content-text");
   JOBAD_IN_PAGE = new JOBAD(selector);
   JOBAD_IN_PAGE.modules.load([
-    "math.showsource",
     "math.clipboard",
     "mathjax.mathjax",
     "wiki.formulapage",
