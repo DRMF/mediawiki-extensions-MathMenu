a15124327745adf97d1f054c04cfd8461ad8f5c7
Deyan Ginev <d.ginev@jacobs-university.de>
2013-08-05 14:47:13 -0400

    can now recognize whether a page is new or already exists

--- wiki.formulapage.js
+++ wiki.formulapage.js
@@ -9,21 +9,28 @@
     contextMenuEntries: function(target){
       var math = target.closest('math');
       if (! math.is('math')) { return false;}
-      
-      return[
-        ["Formula page", function(element){
-          /*     var link = target.closest('a').attr('href');
-          window.location.href = link; */
-          var math_key = 'title';
-          var tex = math.find('annotation[encoding="application/x-tex"]').text();
-          tex = tex.replace(/\[/g,"obrackett").replace(/\]/g,"cbrackett").replace(/\{/g,"obracee").replace(/\}/g,"cbracee");
-          tex = encodeURIComponent(tex);//.replace(/\(/g, "%28").replace(/\\\\/g,"\\").replace(/\)/g,"%29").replace(/\*/g,"%@A").replace(/\\/g,"%5C");        
-          var new_url ="/index.php?title=" + tex+"&action=edit&boilerplate=Template%3ADRMF";
-          //alert(new_url);
-          //alert(decodeURIComponent(math_value));
-          window.location.href = new_url;
-        }]
-      ];
+
+      var tex = math.find('annotation[encoding="application/x-tex"]').text();
+      tex = tex.replace(/\[/g,"obrackett").replace(/\]/g,"cbrackett").replace(/\{/g,"obracee").replace(/\}/g,"cbracee");
+      tex = encodeURIComponent(tex);//.replace(/\(/g, "%28").replace(/\\\\/g,"\\").replace(/\)/g,"%29").replace(/\*/g,"%@A").replace(/\\/g,"%5C");        
+
+      var req = new XMLHttpRequest();
+      req.open('GET', '/index.php?title='+tex, false);
+      req.send(null);
+      $page_exists = (req.status == 200);
+
+      if (! $page_exists) {
+        return[
+          ["New formula page", function(element){
+            window.location.href = "/index.php?title=" + tex+"&action=edit&boilerplate=Template%3ADRMF";
+          }]];
+      }
+      else {
+        return[
+          ["Formula page", function(element){
+            window.location.href = "/index.php?title=" + tex;
+          }]]; 
+      }
     }
   });
 })(JOBAD.refs.$);
